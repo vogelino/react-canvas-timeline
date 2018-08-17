065b6cf18501a8fd5d39e8e6626cb59357e0c9ca
vogelino <contact@vogelino.com>
2018-08-17 16:08:50 +0200

    Refactor to use the same fnc on scroll and on drag

--- src/App.js
+++ src/App.js
@@ -133,6 +133,11 @@ class Timeline extends Component {
 		return false;
 	}
 
+	onMouseWheel({ deltaX }) {
+		const initialScrollLeft = this.scrollLeft + deltaX;
+		this.scrollView({ initialScrollLeft, deltaX });
+	}
+
 	getRubyMouseEnterHandler(onMouseEnter = () => { }) {
 		const update = () => this.canvasApp.view.update();
 		return function onRubyMouseEnter(mouseInfos) {
@@ -163,36 +168,15 @@ class Timeline extends Component {
 	}
 
 	getTimelineDragHandler() {
-		const drawGraphics = this.drawGraphics.bind(this);
-
 		return (event) => {
-			const delta = event.downPoint.subtract(event.point);
-			const initialScrollLeft = this.canvasApp.view.bounds.x;
+			const deltaX = event.downPoint.subtract(event.point).x;
+			const initialScrollLeft = this.scrollLeft + deltaX;
 
-			if (this.scrollLeft + delta.x < 0) {
-				this.canvasApp.view.center = new paper.Point(
-					this.viewWidth / 2,
-					this.viewHeight / 2,
-				);
-				this.scrollLeft = 0;
-			} else if (this.scrollLeft + delta.x > this.canvasWidth - this.viewWidth) {
-				this.canvasApp.view.center = new paper.Point(
-					this.canvasWidth - (this.viewWidth / 2),
-					this.viewHeight / 2,
-				);
-				this.scrollLeft = this.canvasWidth - this.viewWidth;
-			} else {
-				this.scrollLeft = initialScrollLeft + delta.x;
-				delta.y = 0;
-				this.canvasApp.view.scrollBy(delta);
-			}
-
-			drawGraphics();
+			this.scrollView({ initialScrollLeft, deltaX });
 		};
 	}
 
-	onMouseWheel({ deltaX }) {
-		const initialScrollLeft = this.scrollLeft + deltaX;
+	scrollView({ initialScrollLeft, deltaX }) {
 		if (initialScrollLeft < 0) {
 			this.canvasApp.view.center = new paper.Point(
 				this.viewWidth / 2,
